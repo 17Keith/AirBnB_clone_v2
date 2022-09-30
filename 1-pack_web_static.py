38c863d7db89b20d97f0f65924b374e9d226e11c
17Keith <kayomega7@gmail.com>
2022-09-30 16:08:08 +0300

    Static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,27 +1,16 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """Generates a .tgz archive of web_static folder"""
 
 from fabric.api import local
 from datetime import datetime
 
 
-
 def do_pack():
-    """Function to compress directory
-    Return:
-        path to archive on success;
-        None on fail
-    """
-    # Get the current time
-    now = datetime.now()
-    now = now.strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_' + now + '.tgz'
-
-    # Create an archive
-    local('mkdir -p versions/')
-    result = local('tar -cvzf {} web_static/'.format(archive_path))
-
-    # Checks if archiving was successful.
-    if result.succeeded:
-        return archive_path
-    return None
+    """Function to compress files"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
