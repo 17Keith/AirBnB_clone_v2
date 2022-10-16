794c03b2d6e452c1a640f385c72a2ba6fda9ae3b
17Keith <kayomega7@gmail.com>
2022-10-16 22:10:39 +0300

    Web_flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+flask model for route
+"""
+from email.policy import strict
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """hbnb route page"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb route page"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def Cistext(text):
+    """
+        c/text route page
+    """
+    return 'C {:s}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def Pythonis_text(text):
+    """Python Text"""
+    return 'Python {:s}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def isNumber(n):
+    """number/n route page"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
